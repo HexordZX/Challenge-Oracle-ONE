9efb9f708b9e8e948b3b63a00b636a6abce7a07d
Jonathan Cifuentes <jhonymcb123@gmail.com>
2023-01-15 16:27:50 -0600

    Integrando funciones de encriptar/desencriptar, copiar  y validaciones

--- scripts.js
+++ scripts.js
@@ -1,19 +1,62 @@
-const d = document;
+const d = document,
+  noTexto = d.querySelector(".no-texto"),
+  output = d.querySelector(".texto-desencriptado"),
+  captura = d.getElementById("capturador"),
+  mostrar = d.querySelector(".mostrar-texto-desencriptado");
+
+const removeAccents = (str) => {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
 
 function Encriptar() {
-  const capturar = d.getElementById("capturador").value;
-  let desencriptado = "";
-  let vocales = ["a", "e", "i", "o", "u"],
-    codificados = ["ai", "enter", "imes", "ober", "ufat"];
-  console.log(capturar);
+  let capturar = captura.value;
+  capturar = removeAccents(capturar);
+  capturar = capturar.replaceAll(
+    /[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-]/g,
+    ""
+  );
   if (capturar === "") {
     alert("Por favor, ingrese un texto para ser encriptado.");
+  } else {
+    let encriptar = capturar
+      .toLowerCase()
+      .replaceAll("e", "enter")
+      .replaceAll("i", "imes")
+      .replaceAll("a", "ai")
+      .replaceAll("o", "ober")
+      .replaceAll("u", "ufat");
+
+    noTexto.style.display = "none";
+    output.classList.remove("no-display");
+    mostrar.value = encriptar;
   }
 }
 
 function Desencriptar() {
-  const capturar = d.getElementById("capturador").value;
+  let capturar = captura.value;
+  capturar = removeAccents(capturar);
+  capturar = capturar.replaceAll(
+    /[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-]/g,
+    ""
+  );
   if (capturar === "") {
     alert("Por favor, ingrese un texto para ser desencriptado.");
+  } else {
+    let desencriptar = capturar
+      .toLowerCase()
+      .replaceAll("enter", "e")
+      .replaceAll("imes", "i")
+      .replaceAll("ai", "a")
+      .replaceAll("ober", "o")
+      .replaceAll("ufat", "u");
+
+    noTexto.style.display = "none";
+    output.classList.remove("no-display");
+    mostrar.value = desencriptar;
   }
+}
+
+function copiar() {
+  navigator.clipboard.writeText(mostrar.value);
+  alert("¡Copiado al portapapeles!");
 }
